cd04be6c43547510d1ffde88341a1f5421803751
Oleksandra_Anisimova <krollzilla@gmail.com>
2021-09-24 15:50:59 +0300

    reformat code 14-user-defined-functions task

--- 14-user-defined-functions/index.php
+++ 14-user-defined-functions/index.php
@@ -4,7 +4,7 @@
 $login = readline("Enter your login: ");
 $password = readline("Enter your password: ");
 if (!$password || !$login) {
-    die(PHP_EOL . 'Authorization failed. You did not enter login/password'.PHP_EOL);
+    die(PHP_EOL.'Authorization failed. You did not enter login/password'.PHP_EOL);
 }
 $fileName = 'users.json';
 $users = readJson($fileName);
@@ -14,16 +14,16 @@ echo $userDescription;
 function readJson($fileName)
 {
     if (!file_exists($fileName)) {
-        die (PHP_EOL . "Can not read your data from users file. File '$fileName' does not exist!".PHP_EOL);
+        die (PHP_EOL."Can not read your data from users file. File '$fileName' does not exist!".PHP_EOL);
     }
     $jsonFile = file_get_contents($fileName);
     if ($jsonFile === false) {
-        die (PHP_EOL . "Error while reading file '$fileName'.".PHP_EOL);
+        die (PHP_EOL."Error while reading file '$fileName'.".PHP_EOL);
     }
     try {
         $jsonArray = json_decode($jsonFile, true, 512, JSON_THROW_ON_ERROR);
     } catch (JsonException $exception) {
-        die (PHP_EOL . "Error while decoding file '$fileName'.".PHP_EOL);
+        die (PHP_EOL."Error while decoding file '$fileName'.".PHP_EOL);
     }
     return $jsonArray;
 }
@@ -32,13 +32,13 @@ function checkUser($login, $password, $users)
 {
     $user = [];
     foreach ($users as $key => $val) {
-        if ($val['Login'] === $login && $val['Password'] ===$password){
+        if ($val['Login'] === $login && $val['Password'] === $password) {
             $user = $val;
         }
     }
     if (empty($user)) {
-        die (PHP_EOL . "Authorization failed. You entered login/password incorrectly".PHP_EOL);
+        die (PHP_EOL."Authorization failed. You entered login/password incorrectly".PHP_EOL);
     }
-    return PHP_EOL . "Login: $login." . PHP_EOL . "User name: " . $user["Name"] . ". User email: " . $user["Email"] .
-        ". User language: ". $user["Language"] . ". " .  PHP_EOL;
+    return PHP_EOL."Login: $login.".PHP_EOL."User name: ".$user["Name"].". User email: ".$user["Email"].
+        ". User language: ".$user["Language"].". ".PHP_EOL;
 }
