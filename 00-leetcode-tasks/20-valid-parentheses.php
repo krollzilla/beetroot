62dfe78adeea4acb3b1aaf7c9ed84404a23a32bc
Oleksandra_Anisimova <krollzilla@gmail.com>
2021-10-09 10:55:19 +0300

    add other solution 20-valid-parentheses

--- 00-leetcode-tasks/20-valid-parentheses.php
+++ 00-leetcode-tasks/20-valid-parentheses.php
@@ -1,24 +1,58 @@
 <?php
-var_dump(checkIfBalanced("()"));
 
-function checkIfBalanced($expression)
+const OPEN_PARENTHESES = ['(', '[', '{'];
+const PARENTHESES_MAP = [
+    ')' => '(',
+    '}' => '{',
+    ']' => '[',
+];
+
+class Solution
 {
-    $stack = [];
-    $startSymbols = ['{', '(',  '['];
-    $pairs = ['{}', '()', '[]'];
+    function isValid(string $s): bool
+    {
+        $stack = [];
+        $length = strlen($s);
 
-    for ($i = 0, $len = strlen($expression); $i < $len; $i++) {
-        $curr = $expression[$i];
-        if (in_array($curr, $startSymbols, true)) {
-            $stack[] = $curr;
-        } else {
-            $prev = array_pop($stack);
-            $pair = "$prev$curr";
-            if (!in_array($pair, $pairs)) {
-                return false;
+
+        for ($i = 0; $i < $length; $i++) {
+            if ($this->isOpenParentheses($s[$i])) {
+                // $char values: ) ] }
+                $stack[] = $s[$i];
+            } else {
+                // $char values: ( [ {
+                // знаходимо відкриваючу скобку, яка відповідає цій закриваючій
+                $openBracket = PARENTHESES_MAP[$s[$i]];
+                // перевіряємо, чи останній елемент стека відповідає скобці із пункту 1.
+                $lastStackElement = array_pop($stack);
+                if ($openBracket !== $lastStackElement) {
+                    return false;
+                }
             }
+        }
+
+        return empty($stack);
+    }
+
+    function isOpenParentheses($char): bool
+    {
+        return in_array($char, OPEN_PARENTHESES, true);
+    }
+}
+
+/*
+
+function isValid($s)
+    {
+        $chars = ['[]', '()', '{}'];
+        $n = ceil(strlen($s)/2);
+
+        for ($i=0;$i<=$n;$i++) {
+            $s = str_replace($chars, '', $s);
+        }
+        if (empty($s)) {
+            return true;
         }
     }
 
-    return count($stack) === 0;
-}
+ */
